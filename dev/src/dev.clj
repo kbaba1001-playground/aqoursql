cf942a16a5b32386382e4d9fa6a301364b190153
kbaba1001 <kbaba1001@users.noreply.github.com>
2019-10-27 00:18:01 +0900

    define graphQL

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -1,30 +1,63 @@
 (ns dev
   (:refer-clojure :exclude [test])
-  (:require [clojure.repl :refer :all]
-            [fipp.edn :refer [pprint]]
+  (:require [clojure.java.io :as io]
+            [clojure.repl :refer :all]
+            [clojure.spec.alpha :as s]
             [clojure.tools.namespace.repl :refer [refresh]]
-            [clojure.java.io :as io]
+            [com.walmartlabs.lacinia :as lacinia]
             [duct.core :as duct]
             [duct.core.repl :as duct-repl]
             [eftest.runner :as eftest]
+            [fipp.edn :refer [pprint]]
             [integrant.core :as ig]
-            [integrant.repl :refer [clear halt go init prep reset]]
-            [integrant.repl.state :refer [config system]]))
+            [integrant.repl :refer [clear halt go init prep]]
+            [integrant.repl.state :refer [config system]]
+            [orchestra.spec.test :as stest]
+            [venia.core :as venia]))
 
 (duct/load-hierarchy)
 
 (defn read-config []
   (duct/read-config (io/resource "aqoursql/config.edn")))
 
-(defn test []
-  (eftest/run-tests (eftest/find-tests "test")))
+(defn reset []
+  (let [result (integrant.repl/reset)]
+    (with-out-str (stest/instrument))
+    result))
 
-(def profiles
-  [:duct.profile/dev :duct.profile/local])
+;;; unit testing
+
+(defn test 
+  ([]
+   (eftest/run-tests (eftest/find-tests "test")
+                     {:multithread? false}))
+  ([sym]
+   (eftest/run-tests (eftest/find-tests sym)
+                     {:multithread? false})))
+
+;;; DB access
+
+(defn db-run [f & args]
+  (apply f (:duct.database.sql/hikaricp system) args))
+
+;;; GraphQL
+
+(defn q
+  ([query] (q query nil))
+  ([query variables]
+   (lacinia/execute (:aqoursql.graphql/schema system)
+                    (venia/graphql-query query)
+                    variables
+                    {:db (:duct.database.sql/hikaricp system)})))
+
+;;; namespace settings
 
 (clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
 
-(when (io/resource "local.clj")
+(when (io/resource "local.slj")
   (load "local"))
+
+(def profiles
+  [:duct.profile/dev :duct.profile/local])
 
 (integrant.repl/set-prep! #(duct/prep-config (read-config) profiles))
